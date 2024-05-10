81edc72253a978772b50794512c5a67a06088f91
mrbin22 <23cm0122@jec.ac.jp>
2024-05-10 15:11:56 +0900

    Initial Commit

---
+++ IOS202map_22/IOS202map_22App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS202map_22App.swift
+//  IOS202map_22
+//
+//  Created by cmStudent on 2024/05/10.
+//
+
+import SwiftUI
+
+@main
+struct IOS202map_22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
