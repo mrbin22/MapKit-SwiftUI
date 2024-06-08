3fce280cefc0e7bd9a1015b0524d334836b6eb24
mrbin22 <23cm0122@jec.ac.jp>
2024-06-08 13:00:15 +0900

    Filter menuを追加する

---
+++ IOS202map_22/FilterMenuBarView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterMenuBarView.swift
+//  IOS202map_22
+//
+//  Created by cmStudent on 2024/06/07.
+//
+
+import SwiftUI
+
+struct FilterMenuBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterMenuBarView()
+}
---
+++ IOS202map_22/MenuSpotModel.swift
@@ -0,0 +1,16 @@
+//
+//  MenuSpotModel.swift
+//  IOS202map_22
+//
+//  Created by cmStudent on 2024/06/07.
+//
+
+import Foundation
+import MapKit
+
+
+struct MenuSpotModel: Identifiable {
+    let id = UUID().uuidString
+    var title: String
+    var location: CLLocationCoordinate2D
+}
